83add9fd4bb6f20888708bf6767a85e4cefe22aa
Joe <kapppa.joe@gmail.com>
2023-12-04 16:04:43 +0000

    add a working ver of day 04. Store status before try to switch to cabal

--- src/day04.hs
+++ src/day04.hs
@@ -1,56 +1,45 @@
-import Text.ParserCombinators.Parsec
 import Data.List (intersect)
+import Text.ParserCombinators.Parsec
 
-
-data Card = Card {
-    id :: Int,
+data Card = Card
+  { id :: Int,
     winningNumbers :: [Int],
     numbersGot :: [Int]
-} deriving (Show)
-
+  }
+  deriving (Show)
 
 parseNumber :: Parser Int
 parseNumber = do
-    number <- many1 digit
-    many $ char ' '
-    return $ read number
+  number <- many1 digit
+  many $ char ' '
+  return $ read number
 
 parseCard :: Parser Card
 parseCard = do
-    string "Card"
-    spaces
-    cardId <- many1 digit
-    char ':'
-    spaces
-    winningNumbers <- many1 parseNumber
-    string "|"
-    spaces
-    numbersGot <- many1 parseNumber
-    return $ Card (read cardId) winningNumbers numbersGot
-    where spaces = many1 $ char ' '
+  string "Card"
+  spaces
+  cardId <- many1 digit
+  char ':'
+  spaces
+  winningNumbers <- many1 parseNumber
+  string "|"
+  spaces
+  numbersGot <- many1 parseNumber
+  return $ Card (read cardId) winningNumbers numbersGot
+  where
+    spaces = many1 $ char ' '
 
 parseCards :: Parser [Card]
 parseCards = sepBy1 parseCard $ char '\n'
 
-s = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
-parsed = parse parseCard "" s
-
--- main = do
---     case parsed of
---         Right card -> print $ countPoints card
---         Left _ -> print "err"
-
-
 findWins :: Card -> [Int]
-findWins (Card _ ws ns)  = intersect ws ns
+findWins (Card _ ws ns) = intersect ws ns
 
 countPoints :: Card -> Int
-countPoints card = 
-    let wins = length $ findWins card
-        hasWon = wins > 0
-    in
-        if hasWon then 2 ^ (wins - 1) else 0
-        
+countPoints card =
+  let wins = length $ findWins card
+      hasWon = wins > 0
+   in if hasWon then 2 ^ (wins - 1) else 0
 
 day04part01 :: [Card] -> Int
 day04part01 cards = sum [countPoints card | card <- cards]
@@ -58,20 +47,32 @@ day04part01 cards = sum [countPoints card | card <- cards]
 countWins :: [Card] -> [Int]
 countWins = map $ length . findWins
 
+sumTwoLists :: [Integer] -> [Integer] -> [Integer]
+sumTwoLists a b = take maxLen $ zipWith (+) a' b'
+  where
+    maxLen = max (length a) (length b)
+    a' = a ++ repeat 0
+    b' = b ++ repeat 0
 
-day04part02 = countWins
+winCards :: [Card] -> [Integer] -> Int -> [Integer]
+winCards cards cardCounter n
+  | n >= length winCounts = cardCounter
+  | winCounts !! n == 0 = cardCounter
+  | otherwise = sumTwoLists cardCounter cardsWon
+  where
+    winCounts = countWins cards
+    wonHowMany = winCounts !! n :: Int
+    howManyNthCard = cardCounter !! n :: Integer
+    cardsWon' = replicate wonHowMany howManyNthCard
+    padZero = replicate (n + 1) 0
+    cardsWon = padZero ++ cardsWon'
 
-memoize :: (Int -> a) -> (Int -> a)
-memoize f = (map f [0 ..] !!)
-
-winCard :: [Int] -> (Int -> Integer) -> Int -> Integer
-winCard winsList f n
-  | n >= length winsList = 0
-  | winList !! n == 0 = 1
-  | otherwise = 
-
-
-        
+day04part02 :: [Card] -> Integer
+day04part02 cards = sum $ foldl winCards' startingCards [0 .. n]
+  where
+    n = length cards
+    startingCards = replicate n 1
+    winCards' = winCards cards
 
 runWithFile :: (Show a) => Parser t -> (t -> a) -> FilePath -> IO ()
 runWithFile p solver fileName = do
@@ -80,6 +81,4 @@ runWithFile p solver fileName = do
     Left err -> print err
     Right games -> print $ solver games
 
-main = runWithFile parseCards day04part02 "example/04.txt"
-
-
+main = runWithFile parseCards day04part02 "puzzle/04.txt"
