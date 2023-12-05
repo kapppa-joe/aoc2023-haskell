c2b1600fcf3f4059f6d124cc3ecb6dcf3efe5686
Joe <kapppa.joe@gmail.com>
2023-12-05 22:47:02 +0000

    (somehow) tidy up day 1

--- src/Day01.hs
+++ src/Day01.hs
@@ -1,34 +1,41 @@
 import Data.Char (digitToInt, isDigit)
 import Data.List (elemIndex, isPrefixOf, tails)
 import Data.Maybe (catMaybes)
-import Utils (Solution, runSolution, testWithExample)
+import Utils (Solution, runSolution)
 
+--------------
 -- PART ONE
+--------------
+
+type Row = String
 
 day01part1 :: Solution
 day01part1 rows = toInteger $ sum calibrationValues
   where
     calibrationValues = map getCalibrationValue rows
 
-getDigit :: String -> Maybe Int
+getDigit :: Row -> Maybe Int
 getDigit [] = Nothing
 getDigit (x : _)
   | isDigit x = Just $ digitToInt x
   | otherwise = Nothing
 
-getFirstDigit :: String -> Int
+getFirstDigit :: Row -> Int
 getFirstDigit xs = head $ catMaybes maybeDigits
   where
     maybeDigits = map getDigit $ tails xs
 
-getCalibrationValue :: String -> Int
+getCalibrationValue :: Row -> Int
 getCalibrationValue xs = headDigit * 10 + lastDigit
   where
     headDigit = getFirstDigit xs
     lastDigit = getFirstDigit $ reverse xs
 
+--------------
 -- PART TWO
+--------------
 
+digitsInWord :: [String]
 digitsInWord = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
 day01part2 :: Solution
@@ -36,42 +43,40 @@ day01part2 rows = toInteger $ sum calibrationValues
   where
     calibrationValues = map getCalibrationValue' rows
 
-getDigit' :: String -> Maybe Int
+getDigit' :: Row -> Maybe Int
 getDigit' [] = Nothing
-getDigit' v@(x : _)
+getDigit' row@(x : _)
   | isDigit x = Just $ digitToInt x
-  | otherwise = tryGetDigitFromWord v
+  | otherwise = tryParseNumberWord row
 
-extractDigitWord :: String -> Maybe String
-extractDigitWord xs = head' $ filter isPrefixOf' digitsInWord
+findNumberWord :: Row -> Maybe String
+findNumberWord row = safeHead $ filter isPrefixOf' digitsInWord
   where
-    isPrefixOf' word = word `isPrefixOf` xs
-    head' [] = Nothing
-    head' (x : _) = Just x
+    isPrefixOf' word = word `isPrefixOf` row
+    safeHead [] = Nothing
+    safeHead (x : _) = Just x
 
-tryGetDigitFromWord :: String -> Maybe Int
-tryGetDigitFromWord xs =
-  case extractDigitWord xs of
-    Just x -> (+ 1) <$> elemIndex x digitsInWord
-    _ -> Nothing
-
-getFirstDigit' :: String -> Int
-getFirstDigit' s = head $ catMaybes maybeDigits
+tryParseNumberWord :: Row -> Maybe Int
+tryParseNumberWord row = findNumberWord row >>= wordToNumber
   where
-    maybeDigits = map getDigit' $ tails s
+    wordToNumber word = (+ 1) <$> elemIndex word digitsInWord
 
-getLastDigit' :: String -> Int
-getLastDigit' s = head $ catMaybes maybeDigits
+getFirstDigit' :: Row -> Int
+getFirstDigit' row = head $ catMaybes maybeDigits
   where
-    maybeDigits = reverse $ map getDigit' $ tails s
+    maybeDigits = map getDigit' $ tails row
 
-getCalibrationValue' :: String -> Int
-getCalibrationValue' xs = firstDigit * 10 + lastDigit
+getLastDigit' :: Row -> Int
+getLastDigit' row = head $ catMaybes maybeDigits
   where
-    firstDigit = getFirstDigit' xs
-    lastDigit = getLastDigit' xs
+    maybeDigits = reverse $ map getDigit' $ tails row
 
+getCalibrationValue' :: Row -> Int
+getCalibrationValue' row = firstDigit * 10 + lastDigit
+  where
+    firstDigit = getFirstDigit' row
+    lastDigit = getLastDigit' row
+
+main :: IO ()
 main = do
   runSolution 1 day01part2
-
--- testWithExample "01" day01part2
