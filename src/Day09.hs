fd264df1496d7ef145b0858a4ba5ead2e15fba5c
Joe <kapppa.joe@gmail.com>
2023-12-09 16:18:09 +0000

    switch to use megaparsec for Day09

--- src/Day09.hs
+++ src/Day09.hs
@@ -1,7 +1,3 @@
--- import Text.Parsec
--- import Text.ParserCombinators.Parsec (Parser)
--- import Utils (runWithParser)
-
 import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as TextIO (readFile)
@@ -24,29 +20,31 @@ parseNums = sepBy1 parseNum hspace
 parseNumLists :: Parser [[Int]]
 parseNumLists = sepBy1 parseNums eol
 
-main = do
-  text <- TextIO.readFile "example/09.txt"
-  let res = runParser parseNumLists "" text
+
+runWithParser :: (Show a) => Parser t -> (t -> a) -> FilePath -> IO ()
+runWithParser parser solver filename = do
+  input <- TextIO.readFile filename
+  let res = runParser parser "" input
   case res of 
     Left err -> print err
-    Right result -> print result
+    Right parsed -> print $ solver parsed
 
 
--- extendList :: [Int] -> [Int]
--- extendList [] = []
--- extendList [x] = [x]
--- extendList numList@(x : xs)
---   | allSameNumber = x : numList
---   | otherwise = (head diffedList + x) : numList
---   where
---     diffedList = extendList $ zipWith (-) numList (tail numList)
---     allSameNumber = all (== x) xs
+extendList :: [Int] -> [Int]
+extendList [] = []
+extendList [x] = [x]
+extendList numList@(x : xs)
+  | allSameNumber = x : numList
+  | otherwise = (head diffedList + x) : numList
+  where
+    diffedList = extendList $ zipWith (-) numList (tail numList)
+    allSameNumber = all (== x) xs
 
--- day09part1 :: [[Int]] -> Int
--- day09part1 = day09part2 . map reverse
+day09part1 :: [[Int]] -> Int
+day09part1 = day09part2 . map reverse
 
--- day09part2 :: [[Int]] -> Int
--- day09part2 numlists = sum [head $ extendList ns | ns <- numlists]
+day09part2 :: [[Int]] -> Int
+day09part2 numlists = sum [head $ extendList ns | ns <- numlists]
 
--- main :: IO ()
--- main = runWithParser parseNumLists day09part1 "puzzle/09.txt"
+main :: IO ()
+main = runWithParser parseNumLists day09part1 "puzzle/09.txt"
