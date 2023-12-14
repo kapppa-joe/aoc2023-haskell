964f0c4a38fe5ec644b545a291a4c8b95877b29d
Joe <kapppa.joe@gmail.com>
2023-12-14 18:11:26 +0000

    clean up

--- justfile
+++ justfile
@@ -5,3 +5,9 @@ rebuild:
 
 run day:
   cabal exec runghc 'src/Day{{day}}.hs'
+
+compile day:
+  cabal exec ghc 'src/Day{{day}}.hs' -- -O2 -outputdir bin -o 'bin/{{day}}'
+
+clean_dir:
+  rm src/*.o src/*.hi
--- src/Day14.hs
+++ src/Day14.hs
@@ -1,11 +1,15 @@
-import Data.List (intercalate, elemIndex)
+import Data.List (elemIndex, intercalate)
 import qualified Data.Map as Map
 import Data.Maybe (catMaybes)
-import Utils (runSolution, testWithExample)
+import Utils (runSolution)
 
 type Coord = (Int, Int)
 
 type RockMap = Map.Map Coord Char
+
+type CycleLength = Int
+
+type BeforeRepeat = Int
 
 data Direction = North | West | South | East
 
@@ -26,19 +30,19 @@ parsePlatform s =
       parsedMap = Map.fromList [((x, y), obj) | (x, y) <- coords, let obj = s !! (y - 1) !! (x - 1), obj /= '.']
    in Platform parsedMap xBound yBound
 
-rockSlideSingleRow :: [Maybe Char] -> [Maybe Char]
-rockSlideSingleRow [] = []
-rockSlideSingleRow (Just '#' : xs) = Just '#' : rockSlideSingleRow xs
-rockSlideSingleRow v =
+slideOneRow :: [Maybe Char] -> [Maybe Char]
+slideOneRow [] = []
+slideOneRow (Just '#' : xs) = Just '#' : slideOneRow xs
+slideOneRow v =
   let (rolling, rest) = span (/= Just '#') v
       countRoundRocks = length [x | x <- rolling, x == Just 'O']
       rolled = replicate (length rolling - countRoundRocks) Nothing ++ replicate countRoundRocks (Just 'O')
-   in rolled ++ rockSlideSingleRow rest
+   in rolled ++ slideOneRow rest
 
 newRockPositionsInRow :: RockMap -> [Coord] -> [(Coord, Char)]
 newRockPositionsInRow m coords =
   let row = [Map.lookup coord m | coord <- coords] :: [Maybe Char]
-      afterSlide = rockSlideSingleRow row
+      afterSlide = slideOneRow row
       filterEmpty coord maybeRock = case maybeRock of
         Just rock -> Just (coord, rock)
         Nothing -> Nothing
@@ -52,10 +56,6 @@ listCoordsTowards direction (Platform _ xBound yBound) =
     East -> [[(x, y) | x <- [1 .. xBound]] | y <- [1 .. yBound]]
     West -> [[(x, y) | x <- reverse [1 .. xBound]] | y <- [1 .. yBound]]
 
-listCoordsTowardsNorth :: Platform -> [[Coord]]
-listCoordsTowardsNorth (Platform _ xBound yBound) =
-  [[(x, y) | y <- reverse [1 .. yBound]] | x <- [1 .. xBound]]
-
 tilt :: Direction -> Platform -> Platform
 tilt direction p@(Platform r x y) =
   let coordLists = listCoordsTowards direction p
@@ -64,53 +64,41 @@ tilt direction p@(Platform r x y) =
 
 totalLoad :: Platform -> Int
 totalLoad (Platform m _ yBound) =
-  let roundRocksYCoords = [y | ((x, y), obj) <- Map.toList m, obj == 'O']
-      load y = yBound - y + 1
-   in sum $ map load roundRocksYCoords
+  let roundRocksYCoords = [y | ((_, y), obj) <- Map.toList m, obj == 'O']
+      loadAt y = yBound - y + 1
+   in sum $ map loadAt roundRocksYCoords
 
 day14part1 :: [String] -> Int
 day14part1 input =
-  let p = parsePlatform input
-      tiltedPlatform = tilt North p
+  let platform = parsePlatform input
+      tiltedPlatform = tilt North platform
    in totalLoad tiltedPlatform
 
 spin :: Platform -> Platform
 spin p =
   foldl (flip tilt) p [North, West, South, East]
 
-findDuplicateCycle :: Eq a => [a] -> Maybe (Int, Int)
-findDuplicateCycle s = findDuplicate' s 0
+findRepeatCycle :: (Eq a) => [a] -> Maybe (BeforeRepeat, CycleLength)
+findRepeatCycle s = findRepeatCycle' s 0
   where
-    findDuplicate' [] _ = Nothing
-    findDuplicate' (x:xs) seen =
+    findRepeatCycle' [] _ = Nothing
+    findRepeatCycle' (x : xs) seen =
       case elemIndex x (take seen s) of
         Just n -> Just (n, seen - n)
-        Nothing -> findDuplicate' xs (seen + 1)
+        Nothing -> findRepeatCycle' xs (seen + 1)
 
-findSameCycle :: Int -> Int -> Int -> Int
-findSameCycle n beforeRepeat repeatCycle =
-  ((n - beforeRepeat) `mod` repeatCycle) + beforeRepeat
+trimRepetition :: Int -> BeforeRepeat -> CycleLength -> Int
+trimRepetition n a b = ((n - a) `mod` b) + a
 
 day14part2 :: [String] -> Int
 day14part2 input =
-  let
-    platform = parsePlatform input
-    spinResults = iterate spin platform
-    dupCycle = findDuplicateCycle spinResults
-  in case dupCycle of
-    Nothing -> error "failed to detect duplication cycle"
-    Just (a, b) -> totalLoad $ spinResults !! findSameCycle 1000000000 a b 
+  let platform = parsePlatform input
+      spinResults = iterate spin platform
+      repeatCycle = findRepeatCycle spinResults
+   in case repeatCycle of
+        Nothing -> error "failed to detect duplication cycle"
+        Just (a, b) -> totalLoad $ spinResults !! trimRepetition 1000000000 a b
 
-
-
-
-trial input =
-  let p = parsePlatform input
-  --  in findDuplicateCycle $ iterate spin p
-  in findSameCycle 1000000000 3 7 
-
---  in take 30 $ zip (map totalLoad (iterate spin p)) [1..]
-
+main :: IO ()
 main = do
   runSolution 14 day14part2
-  -- testWithExample "14" day14part2
