0790df0a466d418265ace5a9b789716a30e00b2f
Joe <kapppa.joe@gmail.com>
2023-12-19 21:23:46 +0000

    add day19 part 1

--- src/Day19.hs
+++ src/Day19.hs
@@ -1,19 +1,20 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Control.Monad.Combinators (sepBy1, sepEndBy1)
+import Control.Monad.Combinators (sepBy1, sepEndBy, sepEndBy1)
+import qualified Data.Array.IArray as IA
 import qualified Data.Map as Map
+import Data.Maybe (fromJust)
 import Text.Megaparsec (choice, eof, some, try)
 import Text.Megaparsec.Char (char, digitChar, eol, letterChar, lowerChar, string)
 import UtilsM (Parser, runWithParser)
-import Control.Monad.Combinators (sepEndBy)
 
-data Parts = Parts {x :: Int, m :: Int, a :: Int, s :: Int} deriving (Eq, Ord, Show)
+type Parts = IA.Array (Int, Int) Int
 
 data Criteria = X | M | A | S deriving (Eq, Ord, Enum, Show)
 
 type Label = String
 
-data Destination = Accept | Reject | Goto Label deriving (Eq, Ord, Show)
+data Result = Accept | Reject | Goto Label deriving (Eq, Ord, Show)
 
 data Condition
   = GreaterThan Criteria Int
@@ -21,50 +22,18 @@ data Condition
   | Else
   deriving (Eq, Ord, Show)
 
-type Rule = (Condition, Destination)
+type Rule = (Condition, Result)
 
 type Workflow = [Rule]
 
 type Workflows = Map.Map Label Workflow
 
-parseWorkflow :: Parser (Label, Workflow)
-parseWorkflow = do
-  label <- some lowerChar
-  _ <- char '{'
-  rules <- sepEndBy (try parseRule) (char ',')
-  endRule <- parseEndRule
-  return (label, rules ++ [endRule])
-
-parseBoth :: Parser (Workflows, [Parts])
+parseBoth :: Parser (Workflows, Parts)
 parseBoth = do
   workflows <- parseWorkflows
   _ <- eol
   parts <- parseParts
   return (workflows, parts)
-
-parseParts :: Parser [Parts]
-parseParts = do
-  parts <- sepBy1 parseSingleParts eol
-  _ <- eof
-  return parts
-  where
-    parseSingleParts :: Parser Parts
-    parseSingleParts = do
-      _ <- string "{x="
-      x <- read <$> some digitChar
-      _ <- string ",m="
-      m <- read <$> some digitChar
-      _ <- string ",a="
-      a <- read <$> some digitChar
-      _ <- string ",s="
-      s <- read <$> some digitChar
-      _ <- char '}'
-      return $ Parts x m a s
-
-parseWorkflows :: Parser Workflows
-parseWorkflows = do
-  workflows <- sepEndBy1 parseWorkflow eol
-  return (Map.fromList workflows)
 
 parseRule :: Parser Rule
 parseRule = do
@@ -96,5 +65,72 @@ parseEndRule = do
         label -> Goto label
   return (Else, dest)
 
+parseWorkflow :: Parser (Label, Workflow)
+parseWorkflow = do
+  label <- some lowerChar
+  _ <- char '{'
+  rules <- sepEndBy (try parseRule) (char ',')
+  endRule <- parseEndRule
+  return (label, rules ++ [endRule])
+
+parseWorkflows :: Parser Workflows
+parseWorkflows = do
+  workflows <- sepEndBy1 parseWorkflow eol
+  return (Map.fromList workflows)
+
+parseParts :: Parser Parts
+parseParts = do
+  partsList <- sepBy1 parseSingleParts eol
+  _ <- eof
+  let len = length partsList
+      parts = IA.listArray ((1, 1), (len, 4)) $ concat partsList
+  return parts
+  where
+    parseSingleParts :: Parser [Int]
+    parseSingleParts = do
+      _ <- string "{x="
+      x <- read <$> some digitChar
+      _ <- string ",m="
+      m <- read <$> some digitChar
+      _ <- string ",a="
+      a <- read <$> some digitChar
+      _ <- string ",s="
+      s <- read <$> some digitChar
+      _ <- char '}'
+      return [x, m, a, s]
+
+pickAccepted :: Workflows -> Parts -> [Int]
+pickAccepted ws parts = filter (judgeByWorkflow "in") [1 .. totalPartsNum]
+  where
+    totalPartsNum = fst . snd $ IA.bounds parts
+
+    judgeByWorkflow :: Label -> Int -> Bool
+    judgeByWorkflow label partNum =
+      let workflow = fromJust $ Map.lookup label ws
+       in case performRule workflow partNum of
+            Accept -> True
+            Reject -> False
+            Goto label' -> judgeByWorkflow label' partNum
+
+    performRule :: [Rule] -> Int -> Result
+    performRule [] _ = error "something went wrong"
+    performRule ((cond, res) : xs) partsNum =
+      case cond of
+        Else -> res
+        GreaterThan crit v -> if partsValue crit > v then res else next
+        LesserThan crit v -> if partsValue crit < v then res else next
+      where
+        partsValue crit = parts IA.! (partsNum, fromEnum crit + 1)
+        next = performRule xs partsNum
+
+day19part1 :: (Workflows, Parts) -> Int
+day19part1 (workflows, parts) = sumPartsRating $ pickAccepted workflows parts
+  where
+    sumPartsRating :: [Int] -> Int
+    sumPartsRating accepted =
+      sum $ [parts IA.! (partNum, cat) | partNum <- accepted, cat <- [1 .. 4]]
+
+
+main :: IO ()
 main = do
-  runWithParser parseBoth id "example/19.txt"
+  runWithParser parseBoth day19part1 "puzzle/19.txt"
