ca6449cd99ac91c71793d15c5a762d0f06dda1c4
Joe <kapppa.joe@gmail.com>
2023-12-24 23:19:18 +0000

    run formatter

--- src/Day01.hs
+++ src/Day01.hs
@@ -7,7 +7,7 @@ data Number = Digit Int | Word Int deriving (Eq, Ord, Show)
 numberWordList :: [String]
 numberWordList = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
-parseInput :: Bool -> [String] ->  [[Number]]
+parseInput :: Bool -> [String] -> [[Number]]
 parseInput considerWord = map parseLine
   where
     parseLine :: String -> [Number]
@@ -20,13 +20,12 @@ parseInput considerWord = map parseLine
           _ -> parseRest
       where
         parseRest = parseLine xs
-        numberWordFound = catMaybes [detectNumberWord str ws | ws <- zip numberWordList [1..9]]
+        numberWordFound = catMaybes [detectNumberWord str ws | ws <- zip numberWordList [1 .. 9]]
 
     detectNumberWord :: String -> (String, Int) -> Maybe Int
     detectNumberWord s (word, n)
       | take (length word) s == word = Just n
       | otherwise = Nothing
-
 
 value :: Number -> Int
 value (Digit x) = x
@@ -34,25 +33,23 @@ value (Word x) = x
 
 calibrationValue :: [Number] -> Int
 calibrationValue [] = error "no digit found"
-calibrationValue v@(x:_) = value x * 10 + getLastDigit v
+calibrationValue v@(x : _) = value x * 10 + getLastDigit v
   where
     getLastDigit [y] = value y
     getLastDigit [] = error "second digit not found"
-    getLastDigit (_:ys) = getLastDigit ys
-
+    getLastDigit (_ : ys) = getLastDigit ys
 
 day01part1 :: [String] -> Int
 day01part1 input = sum $ map calibrationValue numbers
   where
     parseDigitsOnly = parseInput False
-    numbers =  parseDigitsOnly input
+    numbers = parseDigitsOnly input
 
 day01part2 :: [String] -> Int
 day01part2 input = sum $ map calibrationValue numbers
   where
     parseWordsAsWell = parseInput True
     numbers = parseWordsAsWell input
-
 
 main :: IO ()
 main = do
